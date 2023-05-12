754a7fb15d3a2bcb659f87c39bdbb324b8bc819b
Yassine Yazidi <104203113+yazidiyassine@users.noreply.github.com>
2023-05-12 22:58:51 +0100

    configuring keycloak and securing the discovery server routes

--- online_shopping_store/discovery-server/src/main/java/org/oss/SecurityConfig.java
+++ online_shopping_store/discovery-server/src/main/java/org/oss/SecurityConfig.java
@@ -3,13 +3,23 @@ package org.oss;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
+@EnableWebSecurity
 public class SecurityConfig {
+
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
-        httpSecurity.csrf().ignoringRequestMatchers("/eureka/**");
+        httpSecurity
+                .csrf().ignoringRequestMatchers("/eureka/**")
+                .and()
+                .authorizeRequests()
+                .requestMatchers("/eureka/**").permitAll()
+                .anyRequest().authenticated()
+                .and()
+                .httpBasic();
         return httpSecurity.build();
     }
 }
